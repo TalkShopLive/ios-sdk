0bd4765ece07faf77eeffecb7b7d6bd245cc64b1
Mayuri <mayuri@talkshop.live>
2025-03-25 19:11:08 -0400

    [iOS-123] GetProducts - added affiliate link - minor changes for other keys

--- Sources/Talkshoplive/Core/Show/Models/ProductData.swift
+++ Sources/Talkshoplive/Core/Show/Models/ProductData.swift
@@ -17,14 +17,17 @@ public struct GetProductsResponse: Codable {
 // Define the main struct representing the top-level data
 public struct ProductData: Codable {
     public var id: Int?
+    public var productKey: String?
     public var name: String?
     public var sku: String?
     public var description: String?
     public var image: String?
-    public var productSource: String?
     public var affiliateLink: String?
     public var variants: [VariantsData]?
-    private let master: Master?
+    public var minimumPrice: String?
+    public var originalPrice: String?
+    public var source: String?
+    private let master: ProductMaster?
     
     // MARK: Coding Keys
     enum CodingKeys: String, CodingKey {
@@ -33,63 +36,81 @@ public struct ProductData: Codable {
         case sku
         case description
         case image
-        case productSource
         case affiliateLink
         case variants
+        case minimumPrice = "minimum_price"
+        case productKey = "product_key"
+        case originalPrice = "original_price"
+        case source
         case master
     }
     
-    // MARK: Initializers
+    // MARK: Initializer
     public init() {
         id = nil
+        productKey = nil
         name = nil
         sku = nil
         description = nil
         image = nil
-        productSource = nil
         affiliateLink = nil
+        minimumPrice = nil
+        originalPrice = nil
+        source = nil
         variants = nil
         master = nil
     }
     
-    // Custom initializer to handle decoding from JSON
+    // MARK: Decodable Initializer
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        // Decode each property and use nil coalescing to handle optional values
         id = try? container.decodeIfPresent(Int.self, forKey: .id)
+        productKey = try? container.decodeIfPresent(String.self, forKey: .productKey)
         name = try? container.decodeIfPresent(String.self, forKey: .name)
         description = try? container.decodeIfPresent(String.self, forKey: .description)
-        productSource = try? container.decodeIfPresent(String.self, forKey: .productSource)
-        affiliateLink = try? container.decodeIfPresent(String.self, forKey: .affiliateLink)
+        minimumPrice = try? container.decodeIfPresent(String.self, forKey: .minimumPrice)
+        originalPrice = try? container.decodeIfPresent(String.self, forKey: .originalPrice)
+        source = try? container.decodeIfPresent(String.self, forKey: .source)
         variants = try? container.decodeIfPresent([VariantsData].self, forKey: .variants)
 
-        master = try? container.decodeIfPresent(Master.self, forKey: .master)
-        image = master?.images?.first?.attachment?.original
+        master = try? container.decodeIfPresent(ProductMaster.self, forKey: .master)
+        
+        if let master = master,
+           let firstImage = master.images?.first,
+           let attachment = firstImage.attachment {
+            image = attachment.original
+        } else {
+            image = nil
+        }
+        
         sku = master?.sku
+        affiliateLink = master?.affiliateLink
+        originalPrice = master?.originalPrice
     }
-
 }
 
+// MARK: - VariantsData
 public struct VariantsData: Codable {
-    public let id: Int?
-    public let displayPrice: String?
-    public let costCurrency: String?
-    public let exchangeName: String?
-    public let buyerPrice: String?
-    public let isDefault: Bool?
+    public var id: Int?
+    public var displayPrice: String?
+    public var costCurrency: String?
+    public var exchangeName: String?
+    public var buyerPrice: String?
+    public var isDefault: Bool?
     public var sku: String?
+    public var position: Int?
 
-    
     // MARK: Coding Keys
     enum CodingKeys: String, CodingKey {
         case id
         case displayPrice = "display_price"
         case costCurrency = "cost_currency"
-        case exchangeName
-        case buyerPrice
-        case isDefault
+        case exchangeName = "exchange_name"
+        case buyerPrice = "buyer_price"
+        case isDefault = "is_default"
         case sku
+        case position
     }
     
     // MARK: Initializers
@@ -101,13 +122,12 @@ public struct VariantsData: Codable {
         buyerPrice = nil
         isDefault = nil
         sku = nil
+        position = nil
     }
     
-    // Custom initializer to handle decoding from JSON
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        // Decode each property and use nil coalescing to handle optional values
+        
         id = try? container.decodeIfPresent(Int.self, forKey: .id)
         displayPrice = try? container.decodeIfPresent(String.self, forKey: .displayPrice)
         costCurrency = try? container.decodeIfPresent(String.self, forKey: .costCurrency)
@@ -115,6 +135,47 @@ public struct VariantsData: Codable {
         buyerPrice = try? container.decodeIfPresent(String.self, forKey: .buyerPrice)
         isDefault = try? container.decodeIfPresent(Bool.self, forKey: .isDefault)
         sku = try? container.decodeIfPresent(String.self, forKey: .sku)
+        position = try? container.decodeIfPresent(Int.self, forKey: .position)
+    }
+}
+
+// MARK: - ProductMaster
+public struct ProductMaster: Codable {
+    public var id: Int?
+    public var sku: String?
+    public var images: [ImageAttachment]?
+    public var affiliateLink: String?
+    public var externalOfferId: String?
+    public var originalPrice: String?
+    
+    // MARK: Coding Keys
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case images
+        case sku
+        case affiliateLink = "affiliate_link"
+        case externalOfferId = "external_offer_id"
+        case originalPrice = "original_price"
+    }
+    
+    // MARK: Initializers
+    public init() {
+        id = nil
+        sku = nil
+        images = nil
+        affiliateLink = nil
+        externalOfferId = nil
+        originalPrice = nil
     }
 
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try? container.decodeIfPresent(Int.self, forKey: .id)
+        images = try? container.decodeIfPresent([ImageAttachment].self, forKey: .images)
+        sku = try? container.decodeIfPresent(String.self, forKey: .sku)
+        affiliateLink = try? container.decodeIfPresent(String.self, forKey: .affiliateLink)
+        externalOfferId = try? container.decodeIfPresent(String.self, forKey: .externalOfferId)
+        originalPrice = try? container.decodeIfPresent(String.self, forKey: .originalPrice)
+    }
 }
